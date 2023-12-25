a4786a5ce8f02c469c29b907dfe83a1e90de7ea9
Sayakdutt <sayakofficial01@gmail.com>
2023-12-25 21:07:02 +0530

    changed github Link

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
           BizFinder
         </h2>
       </div>
-      <div className="flex items-center gap-3">
-        <Link href="https://www.github.com/Sayakdutt" target="_blank">
-          <FaGithub size={40} className="hover:bg-slate-300 hover:rounded-lg"/>
+      <div className="flex items-center gap-3 ">
+        <Link href="https://www.github.com/Sayakdutt/BizFinder" target="_blank">
+          <FaGithub size={40} className="hover:bg-slate-300 hover:rounded-lg hidden md:visible"/>
         </Link>
         <Link href="https://www.linkedin.com/in/sayak-dutta-b5b971240/" target="_blank">
           <FaLinkedin  size={40} className="text-blue-700 hover:bg-slate-300 hover:rounded-lg" />
